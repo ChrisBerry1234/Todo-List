ceabd3aab02ee8283e4a8006719555d4ea7ace98
ChrisBerry1234 <christopherjberry846@gmail.com>
2025-12-29 18:58:19 -0500

    Added container using .map to display each item in the tododata object

--- TodoListApp/src/App.jsx
+++ TodoListApp/src/App.jsx
@@ -6,9 +6,6 @@ function App() {
   //creating state to hold data
   const [todo, setTodo] = useState([{task:"Gym", completed:false, dataAdded: curr.toLocaleDateString()}]);
 
-
-
-
 return (
   <>
   <div className="Main-Container">
@@ -23,10 +20,18 @@ return (
       <button onClick={AddTask}>Add Task</button>
     </div>
 
-
+    <div className="Todo-List-Container">
+      {todo.map((item, index) => {
+        return (
+          <div key={index} className="Todo-Item">
+            <span>{item.task}</span>
+            <span>{item.dataAdded}</span>
+          </div>
+        );
+      })}   
+    </div>
   </div>
   </>
-)
-};
+)};
 
 export default App;
